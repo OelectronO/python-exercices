98a063fabeaf8541e9d2bdc3a371d9d96f8776c0
OelectronO <96228029+OelectronO@users.noreply.github.com>
2021-12-20 11:47:33 +0100

    Add files via upload

---
+++ exercices9.py
@@ -0,0 +1,14 @@
+first_number =input("Entre un premier nombre")
+second_number =input("Entre un deuxieme nombre")
+
+switch = 0
+
+print("Ton premier nombre est : "+str(first_number)+"\nTon deuxieme nombres est : "+str(second_number)+"\nBeep boop beep")
+
+switch=first_number
+
+first_number=second_number
+
+second_number=switch
+
+print("Ton premier nombre est maintenant : "+str(first_number)+"\nTon deuxieme nombres est : "+str(second_number))
